310af11b743ac84d60346dfe1ec221623f0432ab
Alaa <alahamadneh35@gmail.com>
2025-03-23 20:12:09 +0200

    task9 is done

---
+++ task9/main.py
@@ -0,0 +1,45 @@
+def merge_dicts(dict1, dict2):
+    return {**dict1, **dict2}
+
+def fill_dict(dict_name):
+    while True:
+        key = input("\nEnter a key : ")
+        value = input("Enter a value : ")
+        dict_name[key] = value
+        choice = input("\nEnter 'q' to quit filling: ")
+        if choice == 'q':
+            break
+
+def main():
+    is_running = True
+    dict1= {}
+    dict2= {}
+    while is_running:
+        print("\n" + "*" * 36)
+        print("Merging Dictionaries ")
+        print("*" * 36)
+        choice = input("\nEnter 'q' to quit or 'c' to continue: ")
+        if choice == 'q':
+            is_running = False
+            continue
+        elif choice == 'c':
+            print("\n" + "*" * 36)
+            print("Filling the first dictionary")
+            print("*" * 36)
+            fill_dict(dict1)
+
+            print("\n" + "*" * 36)
+            print("Filling the second dictionary")
+            print("*" * 36)
+            fill_dict(dict2)
+        else:
+            print("invalid choice!")
+
+    merged_dicts= merge_dicts(dict1, dict2)
+    print(f"The merged dictionary is: {merged_dicts}")
+
+
+
+if __name__ == "__main__":
+    main()
+
